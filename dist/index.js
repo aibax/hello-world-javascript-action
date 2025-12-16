052ce0569d279e840e01e3580f4b95cdd3c262f0
Daisuke AIBA <aiba@dreamgarage.jp>
2025-12-16 21:47:36 +0900

    ビルドした成果物を追加

--- dist/index.js
+++ dist/index.js
@@ -32248,18 +32248,17 @@ function requireGithub () {
 var githubExports = requireGithub();
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = coreExports.getInput("who-to-greet");
-  coreExports.info(`Hello ${nameToGreet}!`);
-
-  // Get the current time and set it as an output variable
-  const time = new Date().toTimeString();
-  coreExports.setOutput("time", time);
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(githubExports.context.payload, undefined, 2);
-  coreExports.info(`The event payload: ${payload}`);
-} catch (error) {
-  coreExports.setFailed(error.message);
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = coreExports.getInput("who-to-greet");
+    coreExports.info(`Hello ${nameToGreet}!`);
+    // Get the current time and set it as an output variable
+    const time = new Date().toTimeString();
+    coreExports.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(githubExports.context.payload, undefined, 2);
+    coreExports.info(`The event payload: ${payload}`);
+}
+catch (error) {
+    coreExports.setFailed(error.message);
 }
 //# sourceMappingURL=index.js.map
